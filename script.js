b9faf9e681f14adfd05996dd3bec6953561b746e
John Doe <elizabeth.lenzer1998@gmail.com>
2021-10-22 15:42:59 -0700

    partial

---
+++ script.js
@@ -0,0 +1,12 @@
+function Likes(){
+    var element=document.getElementById('AlertLike');
+    alert("Ninja was liked")
+}
+
+function Definition(element){
+    element.style.display="none"
+}
+
+function LogIn(element){
+    element.innerText='log out'
+}
