e5fd000b33782636e61bae4ef0e774ff9ca3a65a
John Doe <elizabeth.lenzer1998@gmail.com>
2021-10-22 16:56:35 -0700

    Partial Commit

---
+++ likes/script.js
@@ -0,0 +1,20 @@
+var x=16;
+function LikeIncrease1(){
+    var element = document.querySelector("#LikeCount1");
+    element.innerText= x + "Like(s)";
+    x++;
+}
+
+var y=11;
+function LikeIncrease2(){
+    var element = document.querySelector("#LikeCount2");
+    element.innerText= y + "Like(s)";
+    x++;
+}
+
+var z=6;
+function LikeIncrease3(){
+    var element = document.querySelector("#LikeCount3");
+    element.innerText= z + "Like(s)";
+    x++;
+}
